4be76919d318235fe104789cf4af8cc1f4233249
Ivor Prebeg <ivor@mbp.(none)>
2012-07-04 15:06:41 +0200

    kext now able to unload without panic()

---
+++ KeyLog/KeyLogKeys.h
@@ -0,0 +1,14 @@
+//
+//  KeyLogKeys.h
+//  KeyLog
+//
+//  Created by Ivor Prebeg on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef KeyLog_KeyLogKeys_h
+#define KeyLog_KeyLogKeys_h
+
+
+
+#endif
